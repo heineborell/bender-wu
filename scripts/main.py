1c3720bea0c23ee07f68acde3f1be60058812d71
Deniz Deveci <dodeve@gmail.com>
2024-09-30 19:57:48 +0900

    script file changed

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,7 @@ from sympy import log
 from bender_wu import taylor_ser
 
 if __name__ == "__main__":
-    N = 300
+    N = 30
     x = symengine.Symbol("x")
     ts = taylor_ser.taylor(x, x * log(x - 1), 3, N)
     tp = taylor_ser.p_coeff(N, ts)
