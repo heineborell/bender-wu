bbdab3cbc4b2390412be9dd305f0c4a926dd6307
Deniz Deveci <dodeve@gmail.com>
2024-10-07 11:28:24 +0900

    feat:taylor series func

--- scripts/main.py
+++ scripts/main.py
@@ -6,6 +6,9 @@ from bender_wu import taylor_ser
 if __name__ == "__main__":
     N = 30
     x = symengine.Symbol("x")
-    ts = taylor_ser.taylor(x, x * log(x - 1), 3, N)
+    r0 = 1.5
+    ts = taylor_ser.taylor(x, x + log(x - 1), 1.5, N)
     tp = taylor_ser.p_coeff(N, ts)
-    print(ts)
+    tc = taylor_ser.c_coeff(ts, N, tp)
+    tss = symengine.series((1 - 1 / x) * ((6 / x**2) - 3 / x**3), x, n=10)
+    print(tss)
--- tests/test_taylor_ser.py
+++ tests/test_taylor_ser.py
@@ -1,10 +1,10 @@
 from sympy import symbols
 
-from bender_wu.taylor import taylor
+from bender_wu.taylor_ser import taylor
 
 
 def test_taylor():
     x = symbols("x")
-    want = {1: 3, 2: 6, 3: 6, 4: 0}
+    want = {1: 3.0, 2: 6.0, 3: 6.0, 4: 0.0}
     got = taylor(x, x**3, 1, 4)
     assert got == want
