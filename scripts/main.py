5b2b0325d4aa379907d6d6ae285e14506da277f6
Deniz Deveci <dodeve@gmail.com>
2024-10-08 17:28:18 +0900

    refac: turned taylor-ser into class

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,16 @@
 import symengine
 from sympy import log
 
-from bender_wu import taylor_ser
+from bender_wu import seriescoeff
 
 if __name__ == "__main__":
-    N = 40
+    N = 140
     x = symengine.Symbol("x")
+    fg = x + log(x - 1)
     r0 = 1.5
-    ts = taylor_ser.taylor(x, x + log(x - 1), 1.5, N, fac=True)
-    tp = taylor_ser.p_coeff(N, ts)
-    tc = taylor_ser.c_coeff(ts, N, tp)
-    tss = symengine.series((1 - 1 / x) * ((6 / x**2) - 3 / x**3), x, n=10)
-    print(ts)
+    prec = 100
+    sf = seriescoeff.SeriesCoeff("x", r0, fg, N, prec)
+    sf.taylor()
+    # sf.p_coeff()
+    # sf.c_coeff()
+    print(sf.f_n)
---
+++ src/bender_wu/seriescoeff.py
@@ -0,0 +1,73 @@
+import math
+
+import symengine
+
+
+class SeriesCoeff:
+    def __init__(self, var, val, func, N, prec) -> None:
+        self.var = symengine.Symbol(var)
+        self.val = val
+        self.func = func
+        self.N = N
+        self.prec = prec
+        self.f_n = {}  # nth derivatives of f(x)
+        self.f_n_x0 = {}  # nth derivatives, but evaluated at x = x_0
+        self.f_n[1] = symengine.diff(func, var, 1)  # differentiate f(x)
+        self.f_n_x0[1] = self.f_n[1].subs(var, val).n(prec)  # do the substitution
+        self.P = {}
+        self.b_n = {}  # Vector of pre-computed dummy variable values
+        self.c_n = {}
+
+    def taylor(self, fac=False):
+
+        if fac is False:
+
+            for i in range(2, self.N + 1):
+                self.f_n[i] = symengine.diff(self.f_n[i - 1], self.var, 1)
+                # it's more efficient to differentiate the previous derivative
+                # once than to directly ask for the nth derivative
+                self.f_n_x0[i] = self.f_n[i].subs(self.var, self.val).n(self.prec)
+
+        else:
+            for i in range(2, self.N + 1):
+                self.f_n[i] = symengine.diff(self.f_n[i - 1], self.var, 1) / i
+                # it's more efficient to differentiate the previous derivative
+                # once than to directly ask for the nth derivative
+                self.f_n_x0[i] = self.f_n[i].subs(self.var, self.val).n(self.prec)
+
+    def p_coeff(self):
+        # Y should contain N symbolic derivatives, where
+        # Y[1] is the first derivative of f(x),
+        # Y[2] is the second derivative of f(x), etc
+        #
+        # Returns a hash table P: (i,j) -> symbolic equation
+        # where P[(i,j)] = P(i,j) from the paper
+        for j in range(1, self.N + 1):
+            self.P[(j, j)] = self.f_n_x0[1] ** j
+            for k in range(j + 1, self.N + 1):
+                self.P[(j, k)] = 0
+                for m in reversed(range(1, k - j + 1)):
+                    self.P[(j, k)] = (
+                        self.P[(j, k)]
+                        + (m * j - k + j + m)
+                        * self.f_n_x0[m + 1]
+                        / math.factorial(m + 1)
+                        * self.P[(j, k - m)]
+                    )
+                self.P[(j, k)] = self.P[(j, k)] * 1 / (k - j) * 1 / self.f_n_x0[1]
+
+    def c_coeff(self):
+
+        self.b_n[1] = 1 / self.f_n_x0[1]
+
+        self.c_n[1] = self.b_n[1] / math.factorial(1)
+
+        for n in range(2, self.N + 1):
+
+            self.b_n[n] = 0
+            for j in range(1, n):
+                self.b_n[n] = (
+                    self.b_n[n] + self.b_n[j] / math.factorial(j) * self.P[(j, n)]
+                )
+                self.b_n[n] = self.b_n[n] * math.factorial(n) * -1 * self.b_n[1] ** n
+                self.c_n[n] = self.b_n[n] / math.factorial(n)
