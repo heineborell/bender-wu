2cf13a1dda3f90bde73cfbed6fc01145cb1b4f07
Deniz Deveci <dodeve@gmail.com>
2024-10-16 21:25:23 +0900

    recursive substitution tries

--- scripts/symtest.py
+++ scripts/symtest.py
@@ -3,27 +3,29 @@ import symengine
 if __name__ == "__main__":
 
     A = {}
-    nu = 0
+    nu = 1
     eLL = 5
     beta = symengine.symbols("beta")
     aarray = symengine.symarray("a", (eLL + 1, 1 + nu + 3 * eLL))
-    varray = symengine.symarray("v", 20)
+    varray = symengine.symarray("v", eLL + 3)
     # harray = symengine.symarray("h", 20)
     harray = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    earray = symengine.symarray("e", 20)
+    earray = symengine.symarray("e", eLL + 3)
 
     for i, j in enumerate(earray):
         if i % 2 != 0:
             earray[i] = 0
 
     for L in range(0, eLL + 1):
-        for k in range(0, nu + 3 * eLL):
+        for k in range(0, 1 + nu + 3 * eLL):
             A[(L, k)] = aarray[L, k]
     for L in range(0, eLL + 1):
         for k in range(0, nu + 3 * L + 1):
-            if k > nu:
+            if k == nu:
+                A[(0, k)] = 1
+            else:
                 A[(0, k)] = 0
-    for k in range(1, nu + 3 * eLL):
+    for k in range(1, nu + 3 * (eLL + 1)):
         for L in range(0, eLL + 1):
             A[(L, -k)] = 0
     for L in range(0, eLL + 1):
@@ -53,4 +55,26 @@ if __name__ == "__main__":
 
         earray[L + 2] = 1 / (2 * A[(0, nu)] * harray[0]) * earray[L + 2]
 
-    print(earray)
+    for L in range(1, eLL + 1):
+        for k in reversed(range(0, nu)):
+            for n in range(1, L + 1):
+                A[(L, k)] = -(k + 1) * (k + 2) * A[(L, k + 2)]
+                for n in range(1, L + 1):
+                    A[(L, k)] = A[(L, k)] + varray[n + 2] * A[(L - n, k - n - 2)]
+                    for m in range(0, n + 3):
+                        A[(L, k)] = (
+                            A[(L, k)] - harray[m] * earray[n + 2 - m] * A[(L - n, k - m)]
+                        )
+            A[(L, k)] = 1 / (2 * (k - nu) * beta) * A[(L, k)]
+
+    efull = symengine.symarray("e", eLL + 3)
+    for i, j in enumerate(efull):
+        if i % 2 != 0:
+            efull[i] = 0
+    efull = efull[efull != 0]
+    subs_dict = dict(zip(efull, earray[earray != 0]))
+
+    earray = earray[earray != 0]
+    for i in range(len(efull)):
+        earray[i] = symengine.sympify(earray[i].subs(subs_dict))
+    print((earray))
