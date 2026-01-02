564057e85670f3d9015882099aacf946102a29dd
Deniz <dodeve@gmail.com>
2026-01-02 21:42:38 +0900

    trying array and optimisation

---
+++ ginac/mithat_array/engine.h
@@ -0,0 +1,38 @@
+#pragma once
+
+// Disable *all* warnings for GiNaC on Clang (macOS) since macOS is clang is
+// more strict than the Linux g++
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Weverything"
+#endif
+
+#ifdef IN_GINAC
+#include "ginac.h"
+#else
+#include <ginac/ginac.h>
+#endif
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
+
+#include <vector>
+using namespace GiNaC;
+
+inline constexpr int nu{0};
+inline constexpr int expansionOrder{200};
+inline constexpr int lmax{2 * expansionOrder};
+inline int even{0};
+inline int lstep{2};
+
+std::array<ex, expansionOrder + 1> fourierSeries(const ex &func,
+                                                 const symbol &var);
+std::vector<ex> vSeries(const ex &func, const symbol &var);
+std::vector<std::vector<ex>> pCoeff(const std::vector<ex> &arr,
+                                    std::size_t order);
+std::vector<ex> cCoeff(const std::vector<ex> &f_n,
+                       const std::vector<std::vector<ex>> &dict, const ex &func,
+                       const symbol &var, std::size_t order);
+void evenCheck(const std::vector<ex> &f_n_x0);
+std::vector<ex> energy(const std::vector<ex> &f_n_x0, ex &omega);
---
+++ ginac/mithat_array/main.cpp
@@ -0,0 +1,19 @@
+#include "engine.h"
+#include "helper.h"
+#include <ginac/pseries.h>
+
+int main() {
+  Digits = 20;
+  Timer t;
+  symbol r{"ra"};
+  symbol x{"x"};
+  ex potential{(x * x) + (x * x * x * x)};
+  ex omega{sqrt(potential.diff(x, 2)).subs(x == 0)};
+  std::vector<ex> result{vSeries(potential, x)};
+  std::vector<ex> ecoeff{energy(result, omega)};
+
+  printArray(ecoeff, true);
+  std::cout << "Time elapsed: " << t.elapsed() << " seconds\n";
+
+  return 0;
+}
